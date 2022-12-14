6d6525c44b86b7f393fc828d497a96f87378f7cf
Axel Thor Aspelund <axel.aspelund@gmail.com>
2022-12-14 12:18:02 +0000

    d3

---
+++ prof21/d3.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int M,N;
+
+long int a[M][N];
+long int b[N][M];
+
+void afrit(int i, int j) {
+	a[i][j] = b[i][j];
+}
+
+
+int main(int argc, char **argv) {
+	
+	return 0;
+}
